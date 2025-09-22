f46603285931e88d125e00e963fc0f68e03bbdcc
quocIntern <q.le102100@gmail.com>
2025-09-22 11:55:02 -0400

    feat: Connect to MongoDB database

--- healthme-backend/index.js
+++ healthme-backend/index.js
@@ -1,12 +1,20 @@
 // 1. Import the Express tool we just installed.
 const express = require('express');
+const mongoose = require('mongoose');
 
 // 2. Create an instance of our server.
 const app = express();
 
 // 3. Define a "port" number for our server to listen on. 3000 is common.
 const PORT = 3000;
-
+const MONGO_URI = "mongodb+srv://qle17_db_user:rYy1jSEhNaG0ENsx@cluster0.attdos5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+mongoose.connect(MONGO_URI)
+    .then( () => {
+        console.log("MongoDB connected");
+    })
+    .catch((error) => {
+        console.log('MongoDB connection error');
+    });
 // 4. Tell the server what to do when someone visits the main page ('/').
 app.get('/', (req, res) => {
   res.send('HealthMe Backend Server is running!');
