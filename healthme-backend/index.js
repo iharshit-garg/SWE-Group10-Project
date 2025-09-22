c11daf68bcb6bf68d72fca20e3127e2fc0da3def
quocIntern <q.le102100@gmail.com>
2025-09-22 12:22:11 -0400

    feat: Implement user registration endpoint

--- healthme-backend/index.js
+++ healthme-backend/index.js
@@ -1,6 +1,7 @@
 // 1. Import the Express tool we just installed.
 const express = require('express');
 const mongoose = require('mongoose');
+const authRoutes = require('./routes/auth');
 const app = express();
 const PORT = 3000;
 const MONGO_URI = "mongodb+srv://qle17_db_user:rYy1jSEhNaG0ENsx@cluster0.attdos5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
@@ -12,11 +13,11 @@ mongoose.connect(MONGO_URI)
     .catch((error) => {
         console.log('MongoDB connection error');
     });
-
+app.use(express.json());
+app.use('/api/auth', authRoutes);
 app.get('/', (req, res) => {
   res.send('HealthMe Backend Server is running!');
 });
-
 app.listen(PORT, () => {
   console.log(`Server is listening on http://localhost:${PORT}`);
 });
